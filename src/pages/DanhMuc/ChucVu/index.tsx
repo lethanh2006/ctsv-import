ddbd6c1680e86ccacba0842f0d02b17136534add
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2026-03-17 17:01:44 +0700

    revert Chucvu

--- src/pages/DanhMuc/ChucVu/index.tsx
+++ src/pages/DanhMuc/ChucVu/index.tsx
@@ -70,7 +70,7 @@ const ChucVuPage = () => {
       modelName="danhmuc.chucvu"
       title="Chức vụ"
       Form={Form}
-      buttons={{ import: true, minimizeGlobalSearch: true }}
+      buttons={{ import: true }}
     />
   );
 };
