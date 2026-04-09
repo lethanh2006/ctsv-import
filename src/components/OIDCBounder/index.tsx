0e961df4203ad37e43ba948ac04a6cd0e05c9cee
Truong Tuan Anh <truongtuananhkk@gmail.com>
2026-04-09 17:07:18 +0700

    Fix logout

--- src/components/OIDCBounder/index.tsx
+++ src/components/OIDCBounder/index.tsx
@@ -149,7 +149,11 @@ export const OIDCBounder: FC<{ children: React.ReactElement }> & { getActions: (
 	return (
 		<AuthProvider
 			{...oidcConfig}
-			redirect_uri={window.location.pathname.includes('/user') ? window.location.origin : window.location.href}
+			redirect_uri={
+				window.location.pathname.includes('/user')
+					? `${window.location.origin}${APP_CONFIG_BASE_PATH}`
+					: window.location.href
+			}
 		>
 			<OIDCBounder_ {...props} />
 		</AuthProvider>
