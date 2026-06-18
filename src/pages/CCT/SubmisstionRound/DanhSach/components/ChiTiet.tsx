ba871d5a59d201e04023f3f86e2c954212656ed5
Truong Tuan Anh <truongtuananhkk@gmail.com>
2026-06-18 17:57:17 +0700

    Fix

--- src/pages/CCT/SubmisstionRound/DanhSach/components/ChiTiet.tsx
+++ src/pages/CCT/SubmisstionRound/DanhSach/components/ChiTiet.tsx
@@ -182,146 +182,148 @@ const ChiTietMyCCT = (props: any) => {
 		<>
 			{!!record?.myCCT?.status && <CardNoteMyCCT myCCT={record?.myCCT ?? ({} as MyCCT.IRecord)} />}
 
-			<Spin spinning={loading}>
-				<div className='cct-export-area' ref={printRef}>
-					<header className='cct-header'>
-						<div className='header-content-cct'>
-							<div className='header-title'>
-								CO-CURRICULUM RECORD - STUDENT DEVELOPMENT <br /> PORFOLIO
+			<div className='cct-export-area' ref={printRef}>
+				<Spin spinning={loading}>
+					<div className='cct-container'>
+						<header className='cct-header'>
+							<div className='header-content-cct'>
+								<div className='header-title'>
+									CO-CURRICULUM RECORD - STUDENT DEVELOPMENT <br /> PORFOLIO
+								</div>
+								<div className='university-logo'>
+									<img src={bgcct} style={{ height: 60 }} />
+								</div>
 							</div>
-							<div className='university-logo'>
-								<img src={bgcct} style={{ height: 60 }} />
-							</div>
-						</div>
-					</header>
+						</header>
 
-					<main className='cct-body'>
-						<div className='column left-column'>
-							<section className='intro-section'>
-								<p className='intro-text'>
-									<b>Co-curriculum Record - Student Development Porfolio (CCP)</b> is part of VinUniversity’s commitment
-									to developing holistic graduates. This Portfolio documents the co-curricular experiences and
-									developmental activities that students engage in during their time at VinUni, capturing evidence of
-									their competencies, growth, and contributions beyond academic performance.
-								</p>
-								<div className='student-info'>
-									<div className='info-group'>
-										<p>Student name: {record?.myCCT?.name || '--'}</p>
-										<p>Date of Birth: {record?.myCCT?.dob ? dayjs(record.myCCT.dob).format('DD/MM/YYYY') : '--'}</p>
-										<p>Year of Enrollment: {record?.myCCT?.yearOfEnrollment ?? '--'}</p>
+						<main className='cct-body'>
+							<div className='column left-column'>
+								<section className='intro-section'>
+									<p className='intro-text'>
+										<b>Co-curriculum Record - Student Development Porfolio (CCP)</b> is part of VinUniversity’s
+										commitment to developing holistic graduates. This Portfolio documents the co-curricular experiences
+										and developmental activities that students engage in during their time at VinUni, capturing evidence
+										of their competencies, growth, and contributions beyond academic performance.
+									</p>
+									<div className='student-info'>
+										<div className='info-group'>
+											<p>Student name: {record?.myCCT?.name || '--'}</p>
+											<p>Date of Birth: {record?.myCCT?.dob ? dayjs(record.myCCT.dob).format('DD/MM/YYYY') : '--'}</p>
+											<p>Year of Enrollment: {record?.myCCT?.yearOfEnrollment ?? '--'}</p>
+										</div>
+										<div className='info-group'>
+											<p>Student ID: {record?.myCCT?.code || '--'}</p>
+											<p>
+												{`Program: ${record?.myCCT?.program || ''}${
+													record?.myCCT?.college ? ` - ${record?.myCCT?.college}` : ''
+												}`}
+											</p>
+										</div>
 									</div>
-									<div className='info-group'>
-										<p>Student ID: {record?.myCCT?.code || '--'}</p>
-										<p>
-											{`Program: ${record?.myCCT?.program || ''}${
-												record?.myCCT?.college ? ` - ${record?.myCCT?.college}` : ''
-											}`}
-										</p>
+								</section>
+
+								<section className='profile-section'>
+									<div className='section-title gold-text'>
+										<img src={Profile} style={{ width: 25, marginLeft: -4, marginTop: -4 }} /> Student Aspiration
+										Statement
 									</div>
-								</div>
-							</section>
 
-							<section className='profile-section'>
-								<div className='section-title gold-text'>
-									<img src={Profile} style={{ width: 25, marginLeft: -4, marginTop: -4 }} /> Student Aspiration
-									Statement
-								</div>
+									<p className='italic-text'>{record?.myCCT?.selfAspiration ?? '--'}</p>
+								</section>
 
-								<p className='italic-text'>{record?.myCCT?.selfAspiration ?? '--'}</p>
-							</section>
+								<section className='skills-section'>
+									<div className='section-title gold-text'>
+										<img src={Skills} style={{ width: 25, marginLeft: -4, marginTop: -7 }} /> Skills and Competency
+										Development
+									</div>
+									<p className='italic-text'>
+										<i>
+											The following competencies were developed and demonstrated through verified co-curricular
+											activities and experiences.
+										</i>
+									</p>
 
-							<section className='skills-section'>
-								<div className='section-title gold-text'>
-									<img src={Skills} style={{ width: 25, marginLeft: -4, marginTop: -7 }} /> Skills and Competency
-									Development
-								</div>
-								<p className='italic-text'>
-									<i>
-										The following competencies were developed and demonstrated through verified co-curricular activities
-										and experiences.
-									</i>
-								</p>
+									<div className='skills-grid'>
+										{record?.myCCT?.listCompetency?.length ? (
+											record?.myCCT?.listCompetency?.map((skill, index) => (
+												<div key={index} className='skill-item'>
+													{skill}
+												</div>
+											))
+										) : (
+											<div className='skill-item'>--</div>
+										)}
+									</div>
+								</section>
 
-								<div className='skills-grid'>
-									{record?.myCCT?.listCompetency?.length ? (
-										record?.myCCT?.listCompetency?.map((skill, index) => (
-											<div key={index} className='skill-item'>
-												{skill}
-											</div>
-										))
+								<section className='projects-section'>
+									<h2 className='section-title gold-text'>
+										<img src={Skills} style={{ width: 25, marginLeft: -4, marginTop: -4 }} /> Featured
+										Projects/Experience
+									</h2>
+
+									{selectedActivities.length ? (
+										selectedActivities.map((item) => renderItem(item))
 									) : (
-										<div className='skill-item'>--</div>
+										<div className='project-item'>--</div>
 									)}
-								</div>
-							</section>
+								</section>
+							</div>
 
-							<section className='projects-section'>
-								<h2 className='section-title gold-text'>
-									<img src={Skills} style={{ width: 25, marginLeft: -4, marginTop: -4 }} /> Featured Projects/Experience
-								</h2>
+							<div className='column right-column'>
+								<section className='awards-section'>
+									<h2 className='section-title gold-text'>
+										<img src={Skills} style={{ width: 25, marginLeft: -7 }} /> Awards, Recognization and Scholarship
+									</h2>
 
-								{selectedActivities.length ? (
-									selectedActivities.map((item) => renderItem(item))
-								) : (
-									<div className='project-item'>--</div>
-								)}
-							</section>
-						</div>
+									{selectedAwards.length ? (
+										selectedAwards.map((item) => renderAwards(item))
+									) : (
+										<div className='project-item'>--</div>
+									)}
+								</section>
 
-						<div className='column right-column'>
-							<section className='awards-section'>
-								<h2 className='section-title gold-text'>
-									<img src={Skills} style={{ width: 25, marginLeft: -7 }} /> Awards, Recognization and Scholarship
-								</h2>
-
-								{selectedAwards.length ? (
-									selectedAwards.map((item) => renderAwards(item))
-								) : (
-									<div className='project-item'>--</div>
-								)}
-							</section>
-
-							<section className='footer-legend'>
-								<div className='end-transcript-divider'>
-									<span>END OF PORTFOLIO</span>
-								</div>
-								<p className='italic-text' style={{ fontSize: 7 }}>
-									<i>
-										The following role classifications describe the level of responsibility and impact demonstrated by
-										the student in each activity:
-									</i>
-								</p>
-
-								<div className='legend-table'>
-									<div className='legend-row'>
-										<span className='symbol'>▶</span>
-										<span className='label'>Participant</span>
-										<span className='desc'>
-											<i>Engages in activities and contributes to implementation</i>
-										</span>
+								<section className='footer-legend'>
+									<div className='end-transcript-divider'>
+										<span>END OF PORTFOLIO</span>
 									</div>
-									<div className='legend-row'>
-										<span className='symbol'>▶▶</span>
-										<span className='label'>Contributor</span>
-										<span className='desc'>
-											<i>Demonstrates initiative by contributing ideas and owning specific outputs.</i>
-										</span>
-									</div>
-									<div className='legend-row'>
-										<span className='symbol'>▶▶▶</span>
-										<span className='label'>Leader</span>
-										<span className='desc'>
-											<i>Provides direction, mobilizes people, and delivers results.</i>
-										</span>
-									</div>
-									<div className='legend-row'>
-										<span className='symbol'>▶▶▶▶</span>
-										<span className='label'>Impact Driver</span>
-										<span className='desc'>
-											<i>Creates scalable impact that extends beyond the original project or context.</i>
-										</span>
-									</div>
-									{/* {record?.levelOfEngagement
+									<p className='italic-text' style={{ fontSize: 7 }}>
+										<i>
+											The following role classifications describe the level of responsibility and impact demonstrated by
+											the student in each activity:
+										</i>
+									</p>
+
+									<div className='legend-table'>
+										<div className='legend-row'>
+											<span className='symbol'>▶</span>
+											<span className='label'>Participant</span>
+											<span className='desc'>
+												<i>Engages in activities and contributes to implementation</i>
+											</span>
+										</div>
+										<div className='legend-row'>
+											<span className='symbol'>▶▶</span>
+											<span className='label'>Contributor</span>
+											<span className='desc'>
+												<i>Demonstrates initiative by contributing ideas and owning specific outputs.</i>
+											</span>
+										</div>
+										<div className='legend-row'>
+											<span className='symbol'>▶▶▶</span>
+											<span className='label'>Leader</span>
+											<span className='desc'>
+												<i>Provides direction, mobilizes people, and delivers results.</i>
+											</span>
+										</div>
+										<div className='legend-row'>
+											<span className='symbol'>▶▶▶▶</span>
+											<span className='label'>Impact Driver</span>
+											<span className='desc'>
+												<i>Creates scalable impact that extends beyond the original project or context.</i>
+											</span>
+										</div>
+										{/* {record?.levelOfEngagement
 										?.sort((a, b) => a.level.order - b.level.order)
 										?.map((item) => (
 											<div className='legend-row' key={item.level._id}>
@@ -333,31 +335,32 @@ const ChiTietMyCCT = (props: any) => {
 												</span>
 											</div>
 										))} */}
-								</div>
+									</div>
 
-								<div className='signatures'>
-									<div className='issue-info'>
-										<p>
-											<i>Serial Number</i>: {record?.myCCT?.serialNumber || ' --'}
-										</p>
-										<p>
-											<i>Date of Issue</i>: {record?.myCCT?.dateOfIssue ?? '--'}
-										</p>
+									<div className='signatures'>
+										<div className='issue-info'>
+											<p>
+												<i>Serial Number</i>: {record?.myCCT?.serialNumber || ' --'}
+											</p>
+											<p>
+												<i>Date of Issue</i>: {record?.myCCT?.dateOfIssue ?? '--'}
+											</p>
+										</div>
+										<div className='provost-sig'>
+											<div>PROVOST OF VINUNIVERSITY</div>
+											<div className='sig-name'>Prof. Tan Yap Peng</div>
+										</div>
 									</div>
-									<div className='provost-sig'>
-										<div>PROVOST OF VINUNIVERSITY</div>
-										<div className='sig-name'>Prof. Tan Yap Peng</div>
-									</div>
-								</div>
-							</section>
+								</section>
+							</div>
+						</main>
+
+						<div className='title-bottom'>
+							<i>This Portfolio is officially issued and verified by VinUniversity</i>
 						</div>
-					</main>
-
-					<div className='title-bottom'>
-						<i>This Portfolio is officially issued and verified by VinUniversity</i>
 					</div>
-				</div>
-			</Spin>
+				</Spin>
+			</div>
 
 			<div className='form-footer'>
 				{!ssoId && (
