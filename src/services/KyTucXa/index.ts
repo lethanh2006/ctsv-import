657dd0c8bcc098c9c06cdfc84c8ed5a6cc15029f
lethanh2006 <thanhld2006.ptit@gmail.com>
2026-06-26 11:20:04 +0700

    clean code

--- src/services/KyTucXa/index.ts
+++ src/services/KyTucXa/index.ts
@@ -3,8 +3,17 @@ import { ETrangThaiDanhMucChung } from '../DanhMuc/constants';
 import { ipCsvc } from '@/utils/ip';
 
 export async function thongKePhongKTX(condition?: any) {
-	return axios.get(`${ipCsvc}/thong-ke-ktx/phong`, { params: { condition: condition } });
+  return axios.get(`${ipCsvc}/thong-ke-ktx/phong`, { params: { condition: condition } });
 }
+
+export async function thongKeDotTongQuanKTX(condition?: any) {
+  return axios.get(`${ipCsvc}/thong-ke-ktx/dot-tong-quan`, { params: condition });
+}
+
+export async function thongKeDotChiTietKTX(condition?: any) {
+  return axios.get(`${ipCsvc}/thong-ke-ktx/dot-chi-tiet`, { params: condition });
+}
+
 
 export async function kichHoatDanhMucChung(
   id: string,
